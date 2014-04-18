f1f197725f5f85848569b71482698aae59e30940
Alexander Robinson <robinson@fis.ucm.es>
2014-04-18 12:17:42 +0300

    Topography basins.

--- gentopo_GRL.f90
+++ gentopo_GRL.f90
@@ -120,7 +120,7 @@ program gentopo
 
         outfldr = "output/Greenland/"//trim(grid%name)
         
-        call Bamber13_to_grid(outfldr, grid, "Greenland",max_neighbors=20,lat_lim=2.d0)
+        call Bamber13_to_grid(outfldr, grid, "Greenland",max_neighbors=4,lat_lim=2.d0)
 !         call ecmwf_to_grid(   outfldr, grid, "GRL075",   max_neighbors=8, lat_lim=2.d0)
 !         call CERES_to_grid(   outfldr, grid, "Global",   max_neighbors=8, lat_lim=2.d0)
 
