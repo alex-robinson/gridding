a25aeb0a5c92779c6222a185d3688889be4834a0
Alexander Robinson <robinson@fis.ucm.es>
2014-11-10 16:25:27 +0200

    Reprocessing bedmap topography.

--- bedmap2_netcdf.f90
+++ bedmap2_netcdf.f90
@@ -34,16 +34,15 @@ program bedmap2_netcdf
 
     type(grid_class) :: grid 
 
-    double precision, allocatable, dimension(:) :: x1, y1, x2, y2, x3, y3 
+    double precision, allocatable, dimension(:) :: x, y
     double precision, allocatable, dimension(:,:) :: var0, var 
-    double precision :: x0, y0, dx 
-    integer :: i, j, h, k, nx, ny
+    integer :: i, j, h, k
 
     character(len=256) :: fnm, filename_topo, filename_acc, filename_vel 
 
     filename_topo = "output/Antarctica/ANT-1KM_BEDMAP2_topo.nc"
-    filename_acc  = "output/Antarctica/ANT-1KM_BEDMAP2_acc.nc"
-    filename_vel  = "output/Antarctica/ANT-1KM_BEDMAP2_vel.nc"
+!     filename_acc  = "output/Antarctica/ANT-1KM_BEDMAP2_acc.nc"
+!     filename_vel  = "output/Antarctica/ANT-1KM_BEDMAP2_vel.nc"
 
     ! Initialize the output grid
     call grid_init(grid,name="ANT-1KM",mtype="polar stereographic",units="kilometers", &
@@ -74,30 +73,14 @@ program bedmap2_netcdf
 
     end if 
 
-    ! Bedmap surface vectors
-    nx = 6667
-    ny = 6667
-    x0 = -3333.d0
-    y0 = -3333.d0 
-    dx = 1.d0 
-    allocate(x1(nx),y1(ny))
-
-    do i = 1, nx
-        x1(i) = x0 + (i-1)*dx
-    end do 
-    do j = 1, ny
-        y1(j) = y0 + (j-1)*dx
-    end do 
-
-    write(*,*) "range(x1): ",minval(x1),maxval(x1)
-    write(*,*) "range(y1): ",minval(y1),maxval(y1)
-
-    allocate(var0(nx,ny))
+    ! Allocate bedmap dimensions and data array
+    call bedmap2_dims(x,y,var0,x0=-3333.d0,dx=1.d0,nx=6667,y0=-3333.d0,dy=1.d0,ny=6667)
     
     ! Surface elevation
     call bedmap2_read("data/Antarctica/Bedmap2/bedmap2_ascii/bedmap2_surface.txt", &
                       "zs",var0,missing_value=mv)
-    var = var0
+!     var = var0
+    call bedmap2_align(var0=var0,x0=x(1),y0=y(1),var1=var,x1=grid%G%x(1),y1=grid%G%y(1),missing_value=mv)
     call nc_write(filename_topo,"zs",real(var),dim1="xc",dim2="yc",missing_value=real(mv), &
                   units="m",long_name="Surface elevation")
     
@@ -122,23 +105,74 @@ program bedmap2_netcdf
     call nc_write(filename_topo,"mask_ice",int(var),dim1="xc",dim2="yc",missing_value=int(mv), &
                   units="-",long_name="Mask (ice-shelf)")
     
-    ! Rock mask
-    call bedmap2_read("data/Antarctica/Bedmap2/bedmap2_ascii/bedmap2_rockmask.txt", &
-                      "mask_rock",var0,missing_value=mv)
-    var = var0
-    call nc_write(filename_topo,"mask_rock",int(var),dim1="xc",dim2="yc",missing_value=int(mv), &
-                  units="-",long_name="Mask (rock)")
+!     ! Rock mask
+!     call bedmap2_read("data/Antarctica/Bedmap2/bedmap2_ascii/bedmap2_rockmask.txt", &
+!                       "mask_rock",var0,missing_value=mv)
+!     var = var0
+!     call nc_write(filename_topo,"mask_rock",int(var),dim1="xc",dim2="yc",missing_value=int(mv), &
+!                   units="-",long_name="Mask (rock)")
     
-    ! Vostock mask
-    call bedmap2_read("data/Antarctica/Bedmap2/bedmap2_ascii/bedmap2_lakemask_vostok.txt", &
-                      "mask_lake",var0,missing_value=mv)
-    var = var0
-    call nc_write(filename_topo,"mask_lake",int(var),dim1="xc",dim2="yc",missing_value=int(mv), &
-                  units="-",long_name="Mask (lake)")
+!     ! Vostock mask
+!     call bedmap2_read("data/Antarctica/Bedmap2/bedmap2_ascii/bedmap2_lakemask_vostok.txt", &
+!                       "mask_lake",var0,missing_value=mv)
+!     var = var0
+!     call nc_write(filename_topo,"mask_lake",int(var),dim1="xc",dim2="yc",missing_value=int(mv), &
+!                   units="-",long_name="Mask (lake)")
+    
+    
+    ! ==== Rignot velocities
+
+    ! Bedmap2: Rignot velocity
+    !ncols         5602
+    !nrows         5602
+    !xllcorner     -2800500
+    !yllcorner     -2801500
+    !cellsize      1000
+    !NODATA_value  -9999
+
+!     ! Allocate bedmap dimensions and data array
+!     call bedmap2_dims(x,y,var0,x0=-2800.d0,dx=1.d0,nx=5602,y0=-2801.d0,dy=1.d0,ny=5602)
+    
+!     ! Surface elevation
+!     call bedmap2_read("data/Antarctica/Bedmap2/bedmap2_ascii/rignot_velocity_bedmap2_grid.txt", &
+!                       "uv",var0,missing_value=mv)
+! !     var = var0
+!     call bedmap2_align(var0=var0,x0=x(1),y0=y(1),var1=var,x1=grid%G%x(1),y1=grid%G%y(1),missing_value=mv)
+!     call nc_write(filename_topo,"uv",real(var),dim1="xc",dim2="yc",missing_value=real(mv), &
+!                   units="m**s^-1",long_name="Velocity")
     
 
 contains 
 
+    subroutine bedmap2_dims(x,y,var0,x0,dx,nx,y0,dy,ny)
+
+        implicit none 
+
+        double precision, allocatable :: x(:), y(:), var0(:,:)
+        double precision :: x0, dx, y0, dy 
+        integer          :: nx, ny 
+
+        if (allocated(x))    deallocate(x)
+        if (allocated(y))    deallocate(y)
+        if (allocated(var0)) deallocate(var0)
+        
+        allocate(x(nx),y(ny),var0(nx,ny))
+
+        do i = 1, nx
+            x(i) = x0 + (i-1)*dx
+        end do 
+        do j = 1, ny
+            y(j) = y0 + (j-1)*dx
+        end do 
+
+        write(*,*) "Bedmap2 input array information:"
+        write(*,*) "nx, ny:   ", nx, ny 
+        write(*,*) "range(x): ", minval(x), maxval(x)
+        write(*,*) "range(y): ", minval(y), maxval(y)
+        
+        return 
+
+    end subroutine bedmap2_dims
 
     subroutine bedmap2_read(filename,name,var2D,missing_value)
 
@@ -177,5 +211,54 @@ contains
 
     end subroutine bedmap2_read
 
+    subroutine bedmap2_align(var0,x0,y0,var1,x1,y1,missing_value)
+        
+        implicit none 
+
+        double precision :: var0(:,:), var1(:,:)
+        double precision :: x0, y0, x1, y1
+        integer :: nx0,ny0, nx1, ny1, i0, j0, i, j 
+        double precision :: missing_value 
+
+        var1 = missing_value 
+
+        nx0 = size(var0,1)
+        ny0 = size(var0,2)
+        nx1 = size(var1,1)
+        ny1 = size(var1,2)
+
+        if (x1 >= x0 .and. y1 >= y0) then 
+            ! Handles case where destination grid starts after the original grid
+            ! ie, x1 > x0 and y1 > y0
+            i0 = nint(x1-x0)
+            j0 = nint(y1-y0)
+
+            do i = 1, nx0 
+                if (i > nx1) exit 
+                do j = 1, ny0
+                    if (j > ny1) cycle 
+                    var1(i,j) = var0(i0+i,j0+j)
+                end do 
+            end do 
+
+        else 
+            ! Handles case where destination grid starts before the original grid
+            ! ie, x0 > x1 and y0 > y1 
+            i0 = nint(x0-x1)
+            j0 = nint(y0-y1)
+
+            do i = 1, nx1 
+                if (i > nx0) exit 
+                do j = 1, ny1
+                    if (j > ny0) cycle 
+                    var1(i0+i,j0+j) = var0(i,j)
+                end do 
+            end do 
+
+        end if 
+        
+        return
+
+    end subroutine bedmap2_align 
 
 end program 
