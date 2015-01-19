02e58fd7424d5eea28aa74d13053739f984630f8
Alexander Robinson <robinson@fis.ucm.es>
2015-01-19 15:42:03 +0200

    ANT processing 40KM.

--- gentopo_ANT.f90
+++ gentopo_ANT.f90
@@ -20,7 +20,7 @@ program gentopo
     !
     ! =========================================================
 
-    if ( .TRUE. ) then 
+    if ( .FALSE. ) then 
 
         ! ## Define output variable field ##
         call grid_init(grid,name="ANT-20KM",mtype="stereographic",units="kilometers", &
@@ -29,18 +29,18 @@ program gentopo
 
         outfldr = "output/Antarctica/"//trim(grid%name)
 
-!         call bedmap2_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
+        call bedmap2_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
         call bedmap2vel_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
 
-!         call CERES_to_grid(outfldr,grid,"Global",max_neighbors=8, lat_lim=2.d0)
+        call CERES_to_grid(outfldr,grid,"Global",max_neighbors=8, lat_lim=2.d0)
         
-!         call ecmwf_to_grid(outfldr,grid,"ANT075",max_neighbors=8, lat_lim=2.d0)
-!         call ecmwf_to_grid(outfldr,grid,"ANT075",clim_range=[1981,2010])
+        call ecmwf_to_grid(outfldr,grid,"ANT075",max_neighbors=8, lat_lim=2.d0)
+        call ecmwf_to_grid(outfldr,grid,"ANT075",clim_range=[1981,2010])
         
-!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",max_neighbors=20,lat_lim=0.5d0)
-!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2000,2010])
-!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2001,2030])
-!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2071,2100])
+        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",max_neighbors=20,lat_lim=0.5d0)
+        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2000,2010])
+        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2001,2030])
+        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2071,2100])
 
         ! Note: Antartica-c20 doesn't work because some files only contain 239 months of
         !       data while they should all have 240 months (1980-1999)
@@ -56,7 +56,7 @@ program gentopo
     !
     ! =========================================================
 
-    if ( .FALSE. ) then 
+    if ( .TRUE. ) then 
 
         ! ## Define output variable field ##
         call grid_init(grid,name="ANT-40KM",mtype="stereographic",units="kilometers", &
@@ -66,17 +66,17 @@ program gentopo
         outfldr = "output/Antarctica/"//trim(grid%name)
 
 !         call bedmap2_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
-        call bedmap2vel_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
+!         call bedmap2vel_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
 
 !         call CERES_to_grid(outfldr,grid,"Global",max_neighbors=8, lat_lim=2.d0)
         
-!         call ecmwf_to_grid(outfldr,grid,"ANT075",max_neighbors=8, lat_lim=2.d0)
-!         call ecmwf_to_grid(outfldr,grid,"ANT075",clim_range=[1981,2010])
+        call ecmwf_to_grid(outfldr,grid,"ANT075",max_neighbors=8, lat_lim=2.d0)
+        call ecmwf_to_grid(outfldr,grid,"ANT075",clim_range=[1981,2010])
         
-!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",max_neighbors=20,lat_lim=0.5d0)
-!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2000,2010])
-!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2001,2030])
-!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2071,2100])
+        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",max_neighbors=20,lat_lim=0.5d0)
+        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2000,2010])
+        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2001,2030])
+        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2071,2100])
 
         ! Note: Antartica-c20 doesn't work because some files only contain 239 months of
         !       data while they should all have 240 months (1980-1999)
