d289d9f0376b4a4cd3e56a16b3374fedbf3f89bd
Alexander Robinson <robinson@fis.ucm.es>
2015-02-19 18:22:29 +0200

    Ready to try interpolating Rignot basal melt data.

--- gentopo_ANT.f90
+++ gentopo_ANT.f90
@@ -70,19 +70,22 @@ program gentopo
 
 !         call CERES_to_grid(outfldr,grid,"Global",max_neighbors=8, lat_lim=2.d0)
         
-        call ecmwf_to_grid(outfldr,grid,"ANT075",max_neighbors=8, lat_lim=2.d0)
-        call ecmwf_to_grid(outfldr,grid,"ANT075",clim_range=[1981,2010])
+!         call ecmwf_to_grid(outfldr,grid,"ANT075",max_neighbors=8, lat_lim=2.d0)
+!         call ecmwf_to_grid(outfldr,grid,"ANT075",clim_range=[1981,2010])
         
-        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",max_neighbors=20,lat_lim=0.5d0)
-        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2000,2010])
-        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2001,2030])
-        call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2071,2100])
+!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",max_neighbors=20,lat_lim=0.5d0)
+!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2000,2010])
+!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2001,2030])
+!         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2071,2100])
 
         ! Note: Antartica-c20 doesn't work because some files only contain 239 months of
         !       data while they should all have 240 months (1980-1999)
         !       This dataset is not used for now...
 !         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-c20",max_neighbors=20,lat_lim=0.5d0)
 !         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-c20",clim_range=[1980,1999])
+        
+        ! Rignot basal melting data
+        call rignotBM_to_grid(outfldr,grid,"Antarctica-1km",max_neighbors=10, lat_lim=1.d0)
 
     end if 
 
--- gridding_datasets.f90
+++ gridding_datasets.f90
@@ -28,7 +28,8 @@ module gridding_datasets
     public :: RACMO2rot_to_grid
     public :: CERES_to_grid 
     public :: bedmap2_read 
-
+    public :: rignotBM_to_grid
+    
 contains
 
     subroutine Bamber13_to_grid(outfldr,grid,domain,max_neighbors,lat_lim)
@@ -519,6 +520,106 @@ contains
         return
 
     end subroutine bedmap2_read
+
+    subroutine rignotBM_to_grid(outfldr,grid,domain,max_neighbors,lat_lim)
+        ! Convert the variables to the desired grid format and write to file
+        ! =========================================================
+        !
+        !       BASAL MELTING DATA on bedmap2 grid
+        !       Reference: 
+        !           Rignot, E., Jacobs, S., Mouginot, J. and 
+        !           Scheuchl, B., Ice-Shelf Melting Around Antarctica, 
+        !           Science, doi: 10.1126/science.1235798, 2013
+        ! =========================================================
+        
+        implicit none 
+
+        character(len=*) :: domain, outfldr 
+        type(grid_class) :: grid 
+        integer :: max_neighbors 
+        double precision :: lat_lim 
+        character(len=512) :: filename, infldr, prefix  
+
+        type(grid_class)   :: gTOPO
+        character(len=256) :: file_invariant, file_surface, file_prefix(2)
+        type(var_defs), allocatable :: invariant(:), surf(:), pres(:) 
+        double precision, allocatable :: invar(:,:)
+        integer :: plev(9) 
+
+        type(map_class)  :: map 
+        type(var_defs) :: var_now 
+        double precision, allocatable :: outvar(:,:), tmp1(:,:), tmp2(:,:), tmp3(:,:)
+        integer, allocatable          :: outmask(:,:)
+        double precision, allocatable :: zb(:,:), zs(:,:), H(:,:)
+        integer :: nyr, nm, q, k, year, m, i, l, year0, year_switch, n_prefix, n_var 
+
+        ! Define input grid
+        if (trim(domain) .eq. "Antarctica") then 
+            
+            ! Define topography (BEDMAP2/rignot) grid and input variable field
+            call grid_init(gTOPO,name="rignot-10KM",mtype="polar stereographic",units="kilometers",lon180=.TRUE., &
+                   x0=-2800.d0,dx=10.d0,nx=561,y0=-2800.d0,dy=10.d0,ny=561, &
+                   lambda=0.d0,phi=-90.d0,alpha=19.0d0)
+
+            ! Define the input filenames
+            infldr         = "output/Antarctica/"
+            file_invariant = trim(infldr)//"Ant_MeltingRate.nc"
+
+            ! Define the output filename 
+            write(filename,"(a)") trim(outfldr)//"/"//trim(grid%name)// &
+                              "_BMELT.nc"
+
+        else
+
+            write(*,*) "Domain not recognized: ",trim(domain)
+            stop 
+        end if 
+
+        ! Define the variables to be mapped 
+        allocate(invariant(2))
+        call def_var_info(invariant(1),file_invariant,  "melt_actual","bm_actual",units="m*a-1")
+        call def_var_info(invariant(2),file_invariant,  "melt_steadystate","bm_equil",units="m*a-1")
+
+        ! Allocate the input grid variable
+        call grid_allocate(gTOPO,invar)
+
+        ! Allocate tmp array to hold full data (that will be trimmed to smaller size)
+        allocate(tmp1(5601,5601))  ! bedmap2-rignot array
+
+        ! Initialize mapping
+        call map_init(map,gTOPO,grid,max_neighbors=max_neighbors,lat_lim=lat_lim,fldr="maps",load=.TRUE.)
+
+        ! Initialize output variable arrays
+        call grid_allocate(grid,outvar)
+        call grid_allocate(grid,outmask)    
+        
+        ! Initialize the output file
+        call nc_create(filename)
+        call nc_write_dim(filename,"xc",   x=grid%G%x,units="kilometers")
+        call nc_write_dim(filename,"yc",   x=grid%G%y,units="kilometers")
+        call nc_write_dim(filename,"month",x=[1,2,3,4,5,6,7,8,9,10,11,12],units="month")
+        call grid_write(grid,filename,xnm="xc",ynm="yc",create=.FALSE.)
+        
+        ! ## INVARIANT FIELDS ##
+        do i = 1, size(invariant)
+            var_now = invariant(i) 
+            call nc_read(var_now%filename,var_now%nm_in,tmp1,missing_value=missing_value)
+            call thin(invar,tmp1,by=10)
+            where( invar .eq. missing_value ) invar = 0.d0 
+
+            call map_field(map,var_now%nm_in,invar,outvar,outmask,var_now%method,20.d3, &
+                          fill=.TRUE.,missing_value=missing_value)
+            call fill_mean(outvar,missing_value=missing_value)
+            if (var_now%method .eq. "nn") then 
+                call nc_write(filename,var_now%nm_out,nint(outvar),dim1="xc",dim2="yc",units=var_now%units_out)
+            else
+                call nc_write(filename,var_now%nm_out,real(outvar),dim1="xc",dim2="yc",units=var_now%units_out)
+            end if 
+        end do 
+
+        return 
+
+    end subroutine rignotBM_to_grid
 
     subroutine ecmwf_to_grid(outfldr,grid,domain,max_neighbors,lat_lim,clim_range)
         ! Convert the variables to the desired grid format and write to file
