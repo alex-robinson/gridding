17cb9851407e10cf0e756316145964a7d0efcb40
Alexander Robinson <robinson@fis.ucm.es>
2015-03-02 18:33:04 +0200

    climber3a.

--- gentopo_ANT.f90
+++ gentopo_ANT.f90
@@ -90,6 +90,8 @@ program gentopo
     
     ! CLIMBER-3alpha
     call climber3a_to_grid(outfldr,"C3_brutos",grid,domain="lgm_1p7strong",max_neighbors=10,lat_lim=5.d0)
+    call climber3a_to_grid(outfldr,"C3_brutos",grid,domain="lgm_1p7weak",max_neighbors=10,lat_lim=5.d0)
+    call climber3a_to_grid(outfldr,"C3_brutos",grid,domain="present",max_neighbors=10,lat_lim=5.d0)
 
     write(*,*)
     write(*,*) "Regridding program finished."
