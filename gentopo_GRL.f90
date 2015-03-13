f2befd4ae126df0374ee4a7268582de142bbf23c
Alexander Robinson <robinson@fis.ucm.es>
2015-03-13 17:49:27 +0200

    CLIMBER3a Bamber01-20KM

--- gentopo_GRL.f90
+++ gentopo_GRL.f90
@@ -89,9 +89,27 @@ program gentopo
 !     call sedLaske_to_grid(outfldr,grid,"Greenland",max_neighbors=10,lat_lim=2.d0)
 !     call ghfMaule_to_grid(outfldr,grid,"Greenland",max_neighbors=10,lat_lim=2.d0)
 !     call ghfDavies_to_grid(outfldr,grid,"Greenland",max_neighbors=10,lat_lim=2.d0)
-    call ghfShapiro_to_grid(outfldr,grid,"Greenland",max_neighbors=10,lat_lim=2.d0)
+!     call ghfShapiro_to_grid(outfldr,grid,"Greenland",max_neighbors=10,lat_lim=2.d0)
     
 !     call LGMsimpson_to_grid(outfldr,grid,"Greenland",max_neighbors=4,lat_lim=1.d0)
+    
+    ! CLIMBER-3alpha
+    path = "/data/sicopolis/data/CLIMBER3a/Montoya2008/"
+    call climber3a_atm_to_grid(outfldr,"Montoya2008",grid,domain="lgm_1p7strong", &
+                               path_in=path,max_neighbors=10,lat_lim=5.d0)
+    call climber3a_atm_to_grid(outfldr,"Montoya2008",grid,domain="lgm_1p7weak", &
+                               path_in=path,max_neighbors=10,lat_lim=5.d0)
+    call climber3a_atm_to_grid(outfldr,"Montoya2008",grid,domain="present", &
+                               path_in=path,max_neighbors=10,lat_lim=5.d0)
+    
+    path = "/data/sicopolis/data/CLIMBER3a/Montoya2008/"
+    call climber3a_ocn_to_grid(outfldr,"Montoya2008",grid,domain="lgm_1p7strong_ocean", &
+                               path_in=path,max_neighbors=10,lat_lim=5.d0)
+    call climber3a_ocn_to_grid(outfldr,"Montoya2008",grid,domain="lgm_1p7strong_ocean", &
+                               path_in=path,max_neighbors=10,lat_lim=5.d0)
+    call climber3a_ocn_to_grid(outfldr,"Montoya2008",grid,domain="present_ocean", &
+                               path_in=path,max_neighbors=10,lat_lim=5.d0)
+    
 
     write(*,*)
     write(*,*) "Regridding program finished."
