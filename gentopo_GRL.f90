56d7b09f03712e80507ab0edfaade9ae29e0ddad
Alexander Robinson <robinson@fis.ucm.es>
2015-03-17 07:36:58 +0200

    Testing climber interpolation with nng.

--- gentopo_GRL.f90
+++ gentopo_GRL.f90
@@ -101,7 +101,7 @@ program gentopo
 !     call climber3a_atm_to_grid(outfldr,"Montoya2008",grid,domain="lgm_1p7weak", &
 !                                path_in=path,max_neighbors=10,lat_lim=5.d0)
     call climber3a_atm_to_grid(outfldr,"Montoya2008",grid,domain="present", &
-                               path_in=path,sigma=200.d0,max_neighbors=10,lat_lim=5.d0)
+                               path_in=path,sigma=100.d0,max_neighbors=10,lat_lim=5.d0)
     
     path = "/data/sicopolis/data/CLIMBER3a/Montoya2008/"
 !     call climber3a_ocn_to_grid(outfldr,"Montoya2008",grid,domain="lgm_1p7strong_ocean", &
