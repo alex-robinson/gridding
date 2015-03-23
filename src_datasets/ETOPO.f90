922eed2cd4f5cf56487ef99ea9f949f51652ce4d
Alexander Robinson <robinson@fis.ucm.es>
2015-03-23 10:08:56 +0200

    Testing ECMWF.

--- src_datasets/ETOPO.f90
+++ src_datasets/ETOPO.f90
@@ -58,11 +58,12 @@ contains
                               trim(grid%name)//"_TOPO-ETOPO1.nc"
 
         ! Get the input dimensions
-        nx = nc_size(file_in_1,"lon")
-        ny = nc_size(file_in_1,"lat")
+        nx = nc_size(file_in_1,"longitude")
+        ny = nc_size(file_in_1,"latitude")
         allocate(inp%lon(nx),inp%lat(ny))
-        call nc_read(file_in_1,"lon",inp%lon)
-        call nc_read(file_in_1,"lat",inp%lat)
+        call nc_read(file_in_1,"latitude",inp%lon)
+        call nc_read(file_in_1,"latitude",inp%lat)
+        inp%lat = inp%lat(ny:1)
 
         ! Define the input grid
         call grid_init(grid0,name="ETOPO1-020deg",mtype="latlon",units="degrees",lon180=.TRUE., &
