4198515ae75b12baa9ce6309afcd5cee7864129c
Alexander Robinson <robinson@fis.ucm.es>
2015-03-23 08:17:16 +0200

    Processing ETOPO.

--- gentopo_NH.f90
+++ gentopo_NH.f90
@@ -55,9 +55,9 @@ program gentopo
     !
     ! =========================================================
 
-!     call etopo1_to_grid(outfldr,grid,"NH",max_neighbors=4,lat_lim=2.d0)
+    call etopo1_to_grid(outfldr,grid,"NH",max_neighbors=4,lat_lim=2.d0)
 
-    call CERES_to_grid(outfldr,grid,"Global",max_neighbors=8,lat_lim=2.d0)
+!     call CERES_to_grid(outfldr,grid,"Global",max_neighbors=8,lat_lim=2.d0)
 
 !     call ecmwf_to_grid(outfldr, grid,"NH",max_neighbors=8,lat_lim=2.d0)
 !     call ecmwf_to_grid( outfldr,grid,"NH",clim_range=[1981,2010])
