57218c84c23421df1c352091f9fccd8a49e41161
Alexander Robinson <robinson@fis.ucm.es>
2015-04-11 21:02:55 +0300

    Testing AN1-CRUST.

--- gentopo_ANT.f90
+++ gentopo_ANT.f90
@@ -63,9 +63,11 @@ program gentopo
     !
     ! =========================================================
 
-    call bedmap2_to_grid(outfldr,   grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
-    call bedmap2vel_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
-    call bedmap2acc_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
+    call An15litho_to_grid(outfldr,grid,"Antarctica",max_neighbors=5,lat_lim=1.0d0)
+
+!     call bedmap2_to_grid(outfldr,   grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
+!     call bedmap2vel_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
+!     call bedmap2acc_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
 
 !     call CERES_to_grid(outfldr,grid,"Global",max_neighbors=4, lat_lim=2.d0)
     
--- src_datasets/AN1CRUST.f90
+++ src_datasets/AN1CRUST.f90
@@ -17,85 +17,55 @@ contains
         ! Convert the variables to the desired grid format and write to file
         ! =========================================================
         !
-        !       TOPO DATA
-        !       http://sites.uci.edu/morlighem/dataproducts/mass-conservation-dataset/
+        !       AN1-CRUST DATA
+        !       http://www.seismolab.org/model/antarctica/lithosphere/index.html
         ! =========================================================
         
         implicit none 
 
-        character(len=*) :: domain, outfldr 
-        type(grid_class) :: grid 
-        integer :: max_neighbors 
-        double precision :: lat_lim 
-        character(len=512) :: filename 
+        character(len=*)    :: domain, outfldr 
+        type(grid_class)    :: grid 
+        integer             :: max_neighbors 
+        double precision    :: lat_lim 
+        character(len=512)  :: filename 
         character(len=1024) :: desc, ref 
 
         type(grid_class)   :: grid0
         character(len=256) :: file_in
-        type(var_defs), allocatable :: vars(:)
+        double precision, allocatable :: lon(:), lat(:)
         double precision, allocatable :: invar(:,:) 
 
         type(map_class)  :: map 
-        type(var_defs) :: var_now 
         double precision, allocatable :: outvar(:,:), tmp(:,:)
         integer, allocatable          :: outmask(:,:)
-        double precision, allocatable :: zb(:,:), zs(:,:), H(:,:)
-        integer :: q, k, m, i, l, n_var 
-        integer :: thin_by = 10 
-        character(len=128) :: method 
+        
+        integer :: nx, ny
+
+        ! Define the input filenames
+        file_in = "/data/sicopolis/data/Antarctica/AN1-CRUST/AN1-CRUST.grd"
+        desc    = "AN1-CRUST: A Moho depth map"
+        ref     = "Meijian, A., Wiens, D., Yue, Z., Mei, F., Nyblade, A. A., &
+                  &Kanao, M., Yuansheng, L., Maggi, A., Lévêque, J., &
+                  &S-velocity Model and Inferred Moho Topography beneath the Antarctic Plate from Rayleigh Waves, &
+                  &J. Geophys. Res., 120(1),359–383, doi:10.1002/2014JB011332, 2015. \n&
+                  &http://www.seismolab.org/model/antarctica/lithosphere/index.html"
+
+        ! Define the output filename 
+        write(filename,"(a)") trim(outfldr)//"/"//trim(grid%name)// &
+                          "_LITHO-AN15.nc"
 
         ! Define input grid
-        if (trim(domain) .eq. "Greenland") then 
-            
-            ! Define grid and input variable field
-            select case(thin_by)
-                case(10)  ! 150m => 1.5km 
-                    call grid_init(grid0,name="ESPG-3413-1.5KM",mtype="polar_stereographic", &
-                            units="kilometers",lon180=.TRUE., &
-                            x0=-637.925d0,dx=1.5d0,nx=1001,y0=-3349.425d0,dy=1.5d0,ny=1794, &
-                            lambda=-45.d0,phi=70.d0,alpha=7.2d0)
+        nx = nc_size(file_in,"lon")
+        ny = nc_size(file_in,"lat")
 
-                case DEFAULT
-                    write(*,*) "Morlighem14_to_grid:: error: thin_by can only be 10."
-                    stop 
+        allocate(lon(nx),lat(ny))
+        call nc_read(file_in,"lon",lon)
+        call nc_read(file_in,"lat",lat)
 
-            end select 
-
-            ! Define the input filenames
-            file_in = "/data/sicopolis/data/Greenland/Morlighem2014_topo/MCdataset-2014-11-19.nc"
-            desc    = "BedMachine: Greenland dataset based on mass conservation, 19-Nov-2014 (v1.7)"
-            ref     = "Morlighem, M., Rignot, E., Mouginot, J., Seroussi, H. and Larour, E., &
-                      &Deeply incised submarine glacial valleys beneath the Greenland Ice Sheet, &
-                      &Nat. Geosci., 7, 418-422, doi:10.1038/ngeo2167, 2014. \n&
-                      &http://sites.uci.edu/morlighem/dataproducts/mass-conservation-dataset"
-
-            ! Define the output filename 
-            write(filename,"(a)") trim(outfldr)//"/"//trim(grid%name)// &
-                              "_TOPO-M14.nc"
-
-        else
-
-            write(*,*) "Domain not recognized: ",trim(domain)
-            stop 
-        end if 
-
-        ! Define the variables to be mapped 
-        allocate(vars(5))
-        call def_var_info(vars(1),trim(file_in),"bed",      "zb",units="m",long_name="Bedrock elevation")
-        call def_var_info(vars(2),trim(file_in),"surface",  "zs",units="m",long_name="Surface elevation")
-        call def_var_info(vars(3),trim(file_in),"thickness","H",units="m",long_name="Ice thickness")
-        call def_var_info(vars(4),trim(file_in),"errbed",   "zb_err",units="m",long_name="Bedrock / ice thickness error")
-        call def_var_info(vars(5),trim(file_in),"mask",     "mask",units="(0 - 3)", &
-                          long_name="(0 = ocean, 1 = ice-free land, 2 = grounded ice, 3 = floating ice)",method="nn")
+        call grid_init(grid0,name="AN1-CRUST",mtype="latlon",units="degrees",lon180=.TRUE.,x=lon,y=lat)
 
         ! Allocate the input grid variable
         call grid_allocate(grid0,invar)
-
-        ! Allocate tmp array to hold full data (that will be trimmed to smaller size)
-        allocate(tmp(10018,17946))
-
-        ! Initialize mapping
-        call map_init(map,grid0,grid,max_neighbors=max_neighbors,lat_lim=lat_lim,fldr="maps",load=.TRUE.)
 
         ! Initialize output variable arrays
         call grid_allocate(grid,outvar)
@@ -111,44 +81,23 @@ contains
         call nc_write_attr(filename,"Description",desc)
         call nc_write_attr(filename,"Reference",ref)
 
-        ! Make sure the file can be opened 
-        write(*,*) "Reading: ",trim(file_in)
-        write(*,*) "nx = ", nc_size(file_in,"x")
-        write(*,*) "ny = ", nc_size(file_in,"y")
-        
-        ! ## FIELDS ##
-        do i = 1, size(vars)
-            var_now = vars(i) 
-            call nc_read(trim(var_now%filename),var_now%nm_in,tmp,missing_value=mv)
-            call thin(invar,tmp,by=thin_by)
+        ! Initialize mapping
+        call map_init(map,grid0,grid,max_neighbors=max_neighbors,lat_lim=lat_lim,fldr="maps",load=.TRUE.)
 
-            if (trim(var_now%nm_out) .eq. "H" .or. trim(var_now%nm_out) .eq. "zs") then 
-                where( invar .eq. mv ) invar = 0.d0 
-            end if
-            if (trim(var_now%nm_out) .eq. "zb") then 
-                call fill_mean(invar,missing_value=mv,fill_value=-1500.d0)
-            end if 
+        ! ## PROCESS FIELDS ##
 
-            method = "radius"
-            if (trim(var_now%nm_out) .eq. "mask") method = "nn" 
+        call nc_read(file_in,"z",invar,missing_value=mv)
+        where (abs(invar) .gt. 1d6) invar = mv 
 
-            call map_field(map,var_now%nm_in,invar,outvar,outmask,method, &
-                           radius=grid%G%dx*grid%xy_conv*0.75d0,fill=.TRUE.,missing_value=mv)
+        call map_field(map,"z",invar,outvar,outmask,method="nn",fill=.TRUE.,missing_value=mv)
+!         call fill_weighted(outvar,missing_value=mv)
+
+        call nc_write(filename,"H_litho",real(outvar),dim1="xc",dim2="yc",missing_value=real(mv))
             
-            if (var_now%method .eq. "nn") then 
-                call fill_nearest(outvar,missing_value=mv)
-                call nc_write(filename,var_now%nm_out,nint(outvar),dim1="xc",dim2="yc",missing_value=int(mv))
-            else
-                call fill_weighted(outvar,missing_value=mv)
-                call nc_write(filename,var_now%nm_out,real(outvar),dim1="xc",dim2="yc",missing_value=real(mv))
-            end if 
-            
-            ! Write variable metadata
-            call nc_write_attr(filename,var_now%nm_out,"units",var_now%units_out)
-            call nc_write_attr(filename,var_now%nm_out,"long_name",var_now%long_name)
-            call nc_write_attr(filename,var_now%nm_out,"coordinates","lat2D lon2D")
-            
-        end do 
+        ! Write variable metadata
+        call nc_write_attr(filename,"H_litho","units","km")
+        call nc_write_attr(filename,"H_litho","long_name","Lithospheric thickness")
+        call nc_write_attr(filename,"H_litho","coordinates","lat2D lon2D")
 
         return 
 
