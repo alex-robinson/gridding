0d2fc2bad97232717b34630b90c2e487140bec09
Alexander Robinson <robinson@fis.ucm.es>
2016-04-25 17:39:39 +0200

    Antarctic basal melting.

--- src_datasets/Rignot13_BasalMelt.f90
+++ src_datasets/Rignot13_BasalMelt.f90
@@ -69,9 +69,9 @@ contains
         ! Define the variables to be mapped 
         allocate(vars(2))
         call def_var_info(vars(1),file_in,"melt_actual","bm_actual",units="m*a-1", &
-                          long_name="Basal melt rate, actual present day",method="nn")
+                          long_name="Basal melt rate, actual present day",method="radius")
         call def_var_info(vars(2),file_in,"melt_steadystate","bm_equil",units="m*a-1", &
-                          long_name="Basal melt rate, shelf equilibrium",method="nn")
+                          long_name="Basal melt rate, shelf equilibrium",method="radius")
 
         ! Allocate the input grid variable
         call grid_allocate(grid0,invar)
@@ -106,7 +106,7 @@ contains
 !             where( invar .eq. missing_value ) invar = 0.d0 
             where( invar .eq. 0.d0 ) invar = mv
 
-            call map_field(map,var_now%nm_in,invar,outvar,outmask,var_now%method,20.d3, &
+            call map_field(map,var_now%nm_in,invar,outvar,outmask,var_now%method,50.d3, &
                           fill=.TRUE.,missing_value=mv)
             call fill_mean(outvar,missing_value=mv)
 
