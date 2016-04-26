c5d1af88717122b2ac230b0b50b4bb5a8f91fae1
Alexander Robinson <robinson@fis.ucm.es>
2016-04-26 12:20:53 +0200

    Antarctic basal melting.

--- gridder.f90
+++ gridder.f90
@@ -88,14 +88,16 @@ program gridder
 !         call bedmap2acc_to_grid(outfldr,grid,"Antarctica",max_neighbors=20,lat_lim=0.5d0)
 !         call ghfMaule_to_grid(outfldr,  grid,"Antarctica",max_neighbors=4,lat_lim=2.d0)
 !         call nasaBasins_to_grid(outfldr,grid,"Antarctica")
+!         call Rignot13_BasalMelt_to_grid(outfldr,grid,"Antarctica",max_neighbors=10,lat_lim=1.d0, &
+!                                         fill=.TRUE.,sigma=200.d0)
+!         call Rignot13_BasalMelt_to_grid(outfldr,grid,"Antarctica",max_neighbors=10,lat_lim=1.d0, &
+!                                         fill=.TRUE.,sigma=100.d0)
+!         call Rignot13_BasalMelt_to_grid(outfldr,grid,"Antarctica",max_neighbors=10,lat_lim=1.d0, &
+!                                         fill=.TRUE.,sigma=50.d0)
+!         call Rignot13_BasalMelt_to_grid(outfldr,grid,"Antarctica",max_neighbors=10,lat_lim=1.d0, &
+!                                         fill=.TRUE.,sigma=20.d0)
         call Rignot13_BasalMelt_to_grid(outfldr,grid,"Antarctica",max_neighbors=10,lat_lim=1.d0, &
-                                        fill=.TRUE.,sigma=200.d0)
-        call Rignot13_BasalMelt_to_grid(outfldr,grid,"Antarctica",max_neighbors=10,lat_lim=1.d0, &
-                                        fill=.TRUE.,sigma=100.d0)
-        call Rignot13_BasalMelt_to_grid(outfldr,grid,"Antarctica",max_neighbors=10,lat_lim=1.d0, &
-                                        fill=.TRUE.,sigma=50.d0)
-        call Rignot13_BasalMelt_to_grid(outfldr,grid,"Antarctica",max_neighbors=10,lat_lim=1.d0, &
-                                        fill=.TRUE.,sigma=20.d0)
+                                        fill=.TRUE.,sigma=0.d0)
         
 !         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",max_neighbors=20,lat_lim=0.5d0)
 !         call RACMO2rot_to_grid( outfldr, grid, "Antarctica-A1B",clim_range=[2000,2010])
