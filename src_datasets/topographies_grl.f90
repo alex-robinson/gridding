357fca0f0751bb4b20323a99fa9d3d8e094b9d97
Alexander Robinson <robinson@fis.ucm.es>
2016-09-26 14:06:42 +0200

    Testing GRL topo processing.

--- src_datasets/topographies_grl.f90
+++ src_datasets/topographies_grl.f90
@@ -166,7 +166,7 @@ contains
             end if
 
             call map_field(map,var_now%nm_in,invar,outvar,outmask,method, &
-                           radius=grid%G%dx*grid%xy_conv,sigma=grid%G%dx,fill=.TRUE.,missing_value=mv)
+                           radius=grid%G%dx*grid%xy_conv,sigma=grid%G%dx*0.5d0,fill=.TRUE.,missing_value=mv)
             
             if (var_now%method .eq. "nn") then 
                 call nc_write(filename,var_now%nm_out,nint(outvar),dim1="xc",dim2="yc",missing_value=int(mv))
