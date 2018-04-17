e8a7ea1eaf7d55b862cc7c54051585607b9edc88
Alexander Robinson <robinson@fis.ucm.es>
2018-04-17 20:27:55 +0200

    Updated config settings for gridding library. Now Makefile can be configured using config.py script. Only works for aire_gfortran so far.

---
+++ config/Makefile_gridder.mk
@@ -0,0 +1,111 @@
+## Individual libraries or modules ##
+
+$(objdir)/ncio.o: $(libdir)/ncio.f90
+		$(FC) $(DFLAGS) $(FFLAGS) $(INC_NC) -c -o $@ $<
+
+$(objdir)/nml.o: $(libdir)/nml.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/control.o: $(srcdir)/control.f90 $(objdir)/nml.o
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/gridding_datasets.o: $(srcdir)/gridding_datasets.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/regions.o: $(srcdir)/regions.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/generic.o: $(srcdir)/generic.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/AN1CRUST.o: $(srcdir)/AN1CRUST.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/bedmap2.o: $(srcdir)/bedmap2.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/CERES.o: $(srcdir)/CERES.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/climber2.o: $(srcdir)/climber2.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/climber3a.o: $(srcdir)/climber3a.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/davini2015.o: $(srcdir)/davini2015.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/ECMWF.o: $(srcdir)/ECMWF.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/ETOPO.o: $(srcdir)/ETOPO.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/GeothermalHeatFlux.o: $(srcdir)/GeothermalHeatFlux.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/GreenlandVelocity.o: $(srcdir)/GreenlandVelocity.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/grisli_g40.o: $(srcdir)/grisli_g40.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/MAR.o: $(srcdir)/MAR.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/nasaBasins.o: $(srcdir)/nasaBasins.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/RACMO2.o: $(srcdir)/RACMO2.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/Rignot13_BasalMelt.o: $(srcdir)/Rignot13_BasalMelt.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/rtopo.o: $(srcdir)/rtopo.f90 $(objdir)/generic.o
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/sediments.o: $(srcdir)/sediments.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/stratigraphy.o: $(srcdir)/stratigraphy.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/topo_reconstructions.o: $(srcdir)/topo_reconstructions.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+$(objdir)/topographies_grl.o: $(srcdir)/topographies_grl.f90
+		$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
+
+obj_datasets =     $(objdir)/control.o \
+									 $(objdir)/gridding_datasets.o \
+									 $(objdir)/generic.o \
+									 $(objdir)/regions.o \
+									 $(objdir)/AN1CRUST.o \
+									 $(objdir)/bedmap2.o \
+									 $(objdir)/CERES.o \
+									 $(objdir)/climber2.o \
+									 $(objdir)/climber3a.o \
+									 $(objdir)/davini2015.o \
+									 $(objdir)/ECMWF.o \
+									 $(objdir)/ETOPO.o \
+									 $(objdir)/GeothermalHeatFlux.o \
+									 $(objdir)/GreenlandVelocity.o \
+									 $(objdir)/grisli_g40.o \
+									 $(objdir)/MAR.o \
+									 $(objdir)/nasaBasins.o \
+									 $(objdir)/RACMO2.o \
+									 $(objdir)/Rignot13_BasalMelt.o \
+									 $(objdir)/rtopo.o \
+									 $(objdir)/sediments.o \
+									 $(objdir)/stratigraphy.o \
+									 $(objdir)/topo_reconstructions.o \
+									 $(objdir)/topographies_grl.o
+
+obj_datasets_climber = $(objdir)/control.o \
+									 $(objdir)/gridding_datasets.o \
+									 $(objdir)/climber2.o \
+									 $(objdir)/climber3a.o
+
+gridder_libs = $(objdir)/ncio.o $(objdir)/nml.o
