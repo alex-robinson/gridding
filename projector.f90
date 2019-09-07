953486d5825542db20ffa1073a62c79f72fae917
Alexander Robinson <robinson@fis.ucm.es>
2019-09-07 23:23:36 +0300

    Added arguments to projector.

--- projector.f90
+++ projector.f90
@@ -10,10 +10,9 @@ program projector
     character(len=56)  :: mtype  
     real(8) :: lambda, phi 
 
+    integer :: narg 
+    character(len=56)  :: arg_lon, arg_lat 
     real(8) :: x, y, lon, lat 
-
-    write(*,*) 
-
 
     ! GRL domain 
     mtype  = "polar_stereographic"
@@ -22,17 +21,28 @@ program projector
 
     ! Initial points object with projection information 
     call points_init(pts,name="projector",mtype=trim(mtype),units="kilometers", &
-                        lon180=.TRUE.,x=[0.0d0],y=[0.0d0],lambda=lambda,phi=phi,latlon=.FALSE.)
+                        lon180=.TRUE.,x=[0.0d0],y=[0.0d0],lambda=lambda,phi=phi,latlon=.FALSE.,verbose=.FALSE.)
     
+    ! Test lon/lat points
     lon = -48.37d0
     lat =  58.21d0
+
+    ! Get lon/lat from command line 
+    narg = command_argument_count() 
+    if (narg .gt. 0) then 
+        call get_command_argument(1,arg_lon)
+        call get_command_argument(2,arg_lat)
+
+        read(arg_lon,*)  lon
+        read(arg_lat,*)  lat
+    end if 
 
     ! Project from lon/lat => x/y
     call oblimap_projection(lon,lat,x,y,pts%proj)
     x = x/pts%xy_conv
     y = y/pts%xy_conv
 
-    write(*,"(a,2f10.2,a4,2f10.2)") "lon/lat => x/y: ", lon, lat, " == ", x, y  
+    write(*,"(a,2f10.2,a4,2f10.2)") "lonlat to xy: ", lon, lat, " >> ", x, y  
 
 
                 
