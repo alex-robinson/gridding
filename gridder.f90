ba3787af5c61318dfb4f67a26e4f0720bc418fa5
Alexander Robinson <robinson@ucm.es>
2020-10-12 10:57:36 +0200

    ANT-8KM

--- gridder.f90
+++ gridder.f90
@@ -45,8 +45,8 @@ program gridder
     !
     ! =========================================================
     
-    domain    = "Greenland"
-    grid_name = "GRL-4KM"
+    domain    = "Antarctica"
+    grid_name = "ANT-8KM"
     outfldr   = "output/"//trim(domain)//"/"//trim(grid_name)
 
     ! =========================================================
@@ -58,7 +58,7 @@ program gridder
     call domain_definition(grid,grid_name)  
 
     ! Write a regional mask 
-    ! call write_regions(outfldr,grid,domain)
+    call write_regions(outfldr,grid,domain)
     
     ! =========================================================
     !
@@ -73,7 +73,7 @@ program gridder
 !     stop 
 
     ! 2. Perform conservative interpolation to lower resolution 
-    ! call rtopo_to_grid(outfldr,grid,domain)
+    call rtopo_to_grid(outfldr,grid,domain)
 
 !     stop 
 
@@ -86,7 +86,7 @@ program gridder
 !     call ICE5G_to_grid(outfldr,     grid,"Global",max_neighbors=4,lat_lim=2.d0)
 !     call sedLaske_to_grid(outfldr,  grid,"Global",max_neighbors=4,lat_lim=2.d0)
 !     call ghfDavies_to_grid(outfldr, grid,"Global",max_neighbors=4,lat_lim=2.d0)
-    call ghfShapiro_to_grid(outfldr,grid,"Global",max_neighbors=4,lat_lim=2.d0)
+    ! call ghfShapiro_to_grid(outfldr,grid,"Global",max_neighbors=4,lat_lim=2.d0)
     
 !     call ecmwf_to_grid(outfldr,grid,sigma=30.d0,max_neighbors=1,lat_lim=2.d0)
 !     call ecmwf_to_grid(outfldr,grid,clim_range=[1981,2010])
