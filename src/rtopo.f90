cd56b09f07330b547a4b3b4f844388b95d226d21
Alexander Robinson <robinson@ucm.es>
2020-12-04 15:12:31 +0100

    Ready to test rtopo with cdo processing.

--- src/rtopo.f90
+++ src/rtopo.f90
@@ -15,6 +15,109 @@ module rtopo
     public :: rtopo_to_grid 
 
 contains 
+    
+    subroutine rtopo_latlon_to_grid_cdo(outfldr,grid,domain)
+        ! Convert the variables to the desired grid format and write to file
+        ! =========================================================
+        !
+        !       RTOPO DATA
+        !       * These data files are enormous (3.5 Gb). Instead of
+        !       * using mapping routines, that may crash due to memory
+        !       * requirements, the first step will be a quick and dirty
+        !       * nearest neighbor solution to a high resolution, local 
+        !       * grid. 
+        !
+        ! =========================================================
+        ! PATH: /p/projects/megarun/greenrise/datasets/rtopo-2/
+        ! londim = 43201
+        ! latdim = 21601
+        ! lon, lat 
+        ! 
+        ! RTopo-2.0.1_30sec_bedrock_topography.nc : bedrock_topography
+        ! long_name = "ocean bathymetry; surface topography of continents; bedrock topography under grounded or floating ice"
+        ! RTopo-2.0.1_30sec_surface_elevation.nc : surface_elevation
+        ! long_name = "upper ice surface height for the Antarctic and Greenland ice sheets / ice shelves (bedrock topography for ice-free continent; zero for ocean)"
+        ! RTopo-2.0.1_30sec_ice_base_topography.nc : ice_base_topography
+        ! long_name = "ice base topography for the Antarctic and Greenland ice sheets / ice shelves (ice draft for ice shelves and floating glaciers; zero in absence of ice)"
+        ! RTopo-2.0.1_30sec_aux.nc : amask
+        ! long_name = "ice ocean rock mask"
+        
+
+        implicit none 
+
+        character(len=*) :: domain, outfldr 
+        type(grid_class) :: grid
+        double precision :: lat_lim
+        character(len=1024) :: path
+        character(len=512)  :: filename_in, filename  
+        character(len=1024) :: desc, ref 
+        character(len=1024) :: var_name, var_name_out, long_name, units  
+        
+        integer :: nx1, ny1 
+        real(4), allocatable :: var1(:,:)
+        real(4), allocatable :: var2(:,:)
+        character(len=56) :: grid_in_name 
+
+        type(map_scrip_class) :: mps 
+
+        ! ### Define input information #####
+        path = "/p/projects/megarun/greenrise/datasets/rtopo-2"
+
+        grid_in_name = "RTopo-2.0.1_30sec"
+
+        ! 1. Bedrock topography ======================
+
+        filename_in = trim(path)//"/"//trim(grid_in_name)//"_bedrock_topography.nc"
+
+        nx1 = nc_size(filename_in,"lon")
+        ny1 = nc_size(filename_in,"lat")
+        
+
+        ! Assume SCRIP map is already written for RTOPO file 
+        ! (done manually to avoid generating a huge grid object)
+        !call grid_write_cdo_desc_short(grid_in,fldr="maps") 
+        
+        ! Define output grid in grid description file 
+        call grid_write_cdo_desc_short(grid,fldr="maps") 
+        
+        ! Generate SCRIP interpolation weights 
+        call map_scrip_init(mps,grid_in_name,grid%name,fldr="maps",src_nc=filename_in)
+
+        stop 
+
+        desc    = "RTOPO-2.0.1 present-day Earth topography data"
+        ref     = "Schaffer, J., Timmermann, R., Arndt, J. E., Kristensen, S. S., Mayer, C., &
+                  &Morlighem, M., and Steinhage, D.: A global, high-resolution data set of &
+                  &ice sheet topography, cavity geometry, and ocean bathymetry, &
+                  &Earth Syst. Sci. Data, 8, 543-557, &
+                  *https://doi.org/10.5194/essd-8-543-2016, 2016. &
+                  &Data download: https://doi.pangaea.de/10.1594/PANGAEA.856844"
+
+        ! ### Define output information #####
+        
+        ! Allocate output variable
+        call grid_allocate(grid,var1)
+
+        ! Define the output filename 
+        write(filename,"(a)") trim(outfldr)//"/"//trim(grid%name)//"_TOPO-RTOPO-2.0.1-new.nc"
+
+        write(*,*) "Output filename: "//trim(filename)
+        
+        ! Initialize the output file
+        call nc_create(filename)
+        call nc_write_dim(filename,"xc",   x=grid%G%x,units="kilometers")
+        call nc_write_dim(filename,"yc",   x=grid%G%y,units="kilometers")
+        call grid_write(grid,filename,xnm="xc",ynm="yc",create=.FALSE.)
+        
+        ! Write meta data 
+        call nc_write_attr(filename,"Description",desc)
+        call nc_write_attr(filename,"Reference",ref)
+
+
+        return 
+
+    end subroutine rtopo_latlon_to_grid_cdo
+
 
     subroutine rtopo_latlon_to_grid(outfldr,grid,domain)
         ! Convert the variables to the desired grid format and write to file
