cde536b1f602dda125ee817f4f474d3533d87d0b
Alexander Robinson <robinson@ucm.es>
2020-12-05 20:14:52 +0100

    era5: download script prepared to download several years simultaneously.

--- era5/get-era5-monthly-single-levels.py
+++ era5/get-era5-monthly-single-levels.py
@@ -6,34 +6,24 @@ import os, sys, json
 # Load from config file
 info      = json.load(open("era5_config_monthly.json"))
 name      = info["name"]
-area      = info["area"]
-years_num = info["years"]
+area      = None
+year0     = info["year0"]
+year1     = info["year1"]
 
-years = list(map(str, years_num))
-print(years)
+years = list(range(year0,year1+1))
 
-year0 = years[0]    # First year
-year1 = years[-1]   # Last  year
+#years = list(map(str, years_num))
+#year0 = years[0]    # First year
+#year1 = years[-1]   # Last  year
 
 # Make output folder with the name the same as the region
-fldr = "data/era5/"+name 
+fldr = "data/ERA5/"+name 
 if not os.path.exists(fldr):
     os.makedirs(fldr)
 
-# Define output filename
-if year0 == year1: 
-    # Only Downloading one year 
-    filename_download = "{fldr}/era5_{name}_{year0}.nc".format(fldr=fldr,name=name,year0=year0)
-else:
-    # Downloading a range of years
-    filename_download = "{fldr}/era5_{name}_{year0}-{year1}.nc".format(fldr=fldr,name=name,year0=year0,year1=year1)
-
-
 print("Summary:")
 print("name: {name}".format(name=name))
-print("filename_download: {fnm}".format(fnm=filename_download))
-#sys.exit()
-
+print("years: {years}".format(years=years))
 
 vars = [
             '2m_temperature', 'sea_surface_temperature', 'total_precipitation',
@@ -42,20 +32,49 @@ print(vars)
 
 c = cdsapi.Client()
 
-c.retrieve(
-    'reanalysis-era5-single-levels-monthly-means',
-    {
-        'format': 'netcdf',
-        'product_type': 'monthly_averaged_reanalysis',
-        'variable': vars,
-        'month': [
-            '01', '02', '03',
-            '04', '05', '06',
-            '07', '08', '09',
-            '10', '11', '12',
-        ],
-        'time': '00:00',
-        'area': area,
-        'year': years,
-    },
-    filename_download)
+for year_now in years:
+
+    year_now_str = [str(year_now)]
+
+    # Define output filename for this year
+    filename_download = "{fldr}/era5_{name}_{year}.nc".format(fldr=fldr,name=name,year=year_now)
+    filename_download_rhum = "{fldr}/era5_{name}_rhum_{year}.nc".format(fldr=fldr,name=name,year=year_now)
+
+    # Single pressure-level variables
+    c.retrieve(
+        'reanalysis-era5-single-levels-monthly-means',
+        {
+            'format': 'netcdf',
+            'product_type': 'monthly_averaged_reanalysis',
+            'variable': vars,
+            'month': [
+                '01', '02', '03',
+                '04', '05', '06',
+                '07', '08', '09',
+                '10', '11', '12',
+            ],
+            'time': '00:00',
+            'area': area,
+            'year': year_now_str,
+        },
+        filename_download)
+
+    # Relative humidity at 1000 hPa pressure level
+    c.retrieve(
+        'reanalysis-era5-pressure-levels-monthly-means',
+        {
+            'format': 'netcdf',
+            'product_type': 'monthly_averaged_reanalysis',
+            'variable': 'relative_humidity',
+            'pressure_level': '1000',
+            'month': [
+                '01', '02', '03',
+                '04', '05', '06',
+                '07', '08', '09',
+                '10', '11', '12',
+            ],
+            'time': '00:00',
+            'area': area,
+            'year': year_now_str,
+        },
+        filename_download_rhum)
