30ae9937eeac202c765e1ab52358dcd355ba68d8
Alexander Robinson <robinson@ucm.es>
2021-05-30 10:05:29 +0200

    era5: bug fix.

--- era5/run_local_all_years.sh
+++ era5/run_local_all_years.sh
@@ -17,17 +17,19 @@ var='2m_temperature'
 #    ./get-era5-monthly-single-levels.py $var $year
 # done
 
-for v in {0:23}
+for v in {0..23}
 do
-for year in {1979..2020}
-do
-    
-#  # Insert the current year into the slurm submit script as an argument  
-#  sed "s/$search/$year/g" $templatename > $filename
-#
-#  # Submit the job 
-#  sbatch $filename
-   #python get-era5-hourly-local.py $year
-   ./get-era5-monthly-single-levels.py $v $year
-done
+   for year in {1979..2020}
+   do
+       
+   #  # Insert the current year into the slurm submit script as an argument  
+   #  sed "s/$search/$year/g" $templatename > $filename
+   #
+   #  # Submit the job 
+   #  sbatch $filename
+      #python get-era5-hourly-local.py $year
+      ./get-era5-monthly-single-levels.py $v $year
+
+      #echo $v $year
+   done
 done
