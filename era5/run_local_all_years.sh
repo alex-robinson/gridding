7a3142ab4c88fb16764791e0f74c296360d4c3aa
Alexander Robinson <robinson@ucm.es>
2021-05-30 10:00:20 +0200

    era: try to get all vars and years.

--- era5/run_local_all_years.sh
+++ era5/run_local_all_years.sh
@@ -5,6 +5,20 @@ templatename='submit_slurm'
 filename='submit_slurm_now'
 var='2m_temperature'
 
+# for year in {1979..2020}
+# do
+    
+# #  # Insert the current year into the slurm submit script as an argument  
+# #  sed "s/$search/$year/g" $templatename > $filename
+# #
+# #  # Submit the job 
+# #  sbatch $filename
+#    #python get-era5-hourly-local.py $year
+#    ./get-era5-monthly-single-levels.py $var $year
+# done
+
+for v in {0:23}
+do
 for year in {1979..2020}
 do
     
@@ -14,5 +28,6 @@ do
 #  # Submit the job 
 #  sbatch $filename
    #python get-era5-hourly-local.py $year
-   ./get-era5-monthly-single-levels.py $var $year
+   ./get-era5-monthly-single-levels.py $v $year
+done
 done
